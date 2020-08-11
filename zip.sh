c97a4554fe3d625a2738374a6176d3854c227494
naturecodevoid <44983869+naturecodevoid@users.noreply.github.com>
2020-08-11 10:37:51 -0700

    Update zip.sh

--- zip.sh
+++ zip.sh
@@ -1,5 +1,5 @@
 # Zips the executable
 cp -v -f -R dist ctjs-installer-linux
-zip ctjs-installer-linux ctjs-installer-linux/*.app/**/* ctjs-installer-linux/*.app/**/**/*
+zip ctjs-installer-linux ctjs-installer-linux/*
 mkdir zip
 mv ctjs-installer-linux.zip zip
