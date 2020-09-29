c06456a93fb32f5dd6f1245835597b3e6032be5f
naturecodevoid <44983869+naturecodevoid@users.noreply.github.com>
2020-09-29 12:55:14 -0700

    Update zip.bat

--- zip.bat
+++ zip.bat
@@ -1,7 +1,6 @@
 :: Zips the .exe file
-cmd /C "timeout 5 /nobreak"
 robocopy ./dist /S /V /MIR ./ctjs-installer-windows
 mkdir zip
-:: powershell "Compress-Archive ctjs-installer-windows/*.exe ctjs-installer-windows.zip"
+powershell "Compress-Archive ctjs-installer-windows/*.exe ctjs-installer-windows.zip"
 :: move ctjs-installer-windows.zip zip
-move ctjs-installer-windows/*.exe zip
+move ctjs-installer-windows/ctjs-installer-windows.exe zip
