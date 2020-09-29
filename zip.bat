7bb5fe442568a623ecf849f6177607d0d54df11e
naturecodevoid <44983869+naturecodevoid@users.noreply.github.com>
2020-09-29 11:15:51 -0700

    Update zip.bat

--- zip.bat
+++ zip.bat
@@ -3,4 +3,4 @@ robocopy ./dist /S /V /MIR ./ctjs-installer-windows
 mkdir zip
 :: powershell "Compress-Archive ctjs-installer-windows/*.exe ctjs-installer-windows.zip"
 :: move ctjs-installer-windows.zip zip
-move ctjs-installer-windows/*.exe zip
+robocopy /MOVE /V ctjs-installer-windows/*.exe zip
