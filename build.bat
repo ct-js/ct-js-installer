1770b880ee8585cc49eff996fb59e4ed7e97d191
naturecodevoid <44983869+naturecodevoid@users.noreply.github.com>
2020-09-29 11:05:43 -0700

    change executable names

--- build.bat
+++ build.bat
@@ -1,1 +1,1 @@
-python -m PyInstaller main.py --noconsole --onefile --name="ctjs-installer" --add-data="./assets;./assets" --icon="./assets/icon.ico" --noconfirm
+python -m PyInstaller main.py --noconsole --onefile --name="ctjs-installer-windows" --add-data="./assets;./assets" --icon="./assets/icon.ico" --noconfirm
