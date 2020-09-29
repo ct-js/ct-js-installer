8a22b64cdc8a980d53ed2426011d94311e7f6c31
naturecodevoid <44983869+naturecodevoid@users.noreply.github.com>
2020-09-29 12:32:10 -0700

    wut

--- zip.bat
+++ zip.bat
@@ -1,5 +1,5 @@
 :: Zips the .exe file
-timeout /T 5
+timeout 5
 robocopy ./dist /S /V /MIR ./ctjs-installer-windows
 mkdir zip
 :: powershell "Compress-Archive ctjs-installer-windows/*.exe ctjs-installer-windows.zip"
